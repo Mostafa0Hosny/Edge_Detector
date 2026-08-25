684bec66f661ea91d97b1ac51e843a1a2c781806
Mostafa0Hosny <103763009+Mostafa0Hosny@users.noreply.github.com>
2026-08-25 22:19:50 +0300

    Add files via upload

---
+++ Edge Detector/5_Edge_Detector_Moore_Rising_Falling/Edge_Detector_Moore_Rising_Falling _TB.v
@@ -0,0 +1,41 @@
+// Edge Detector
+module Edge_Detector_TB ();
+    reg  CLK_tb;
+    reg  RST_tb;
+    reg  IN_tb;
+    wire Tic_Moore_tb;
+
+
+
+Edge_Detector_Moore_Rising_Falling dut(
+.CLK(CLK_tb),
+.RST(RST_tb),
+.IN(IN_tb),
+.Tic_Moore(Tic_Moore_tb) );
+
+always #5 CLK_tb = ~CLK_tb ;
+
+initial
+begin
+
+ // System Functions
+ $dumpfile("Edge Detector.vcd") ;       
+ $dumpvars; 
+CLK_tb = 1'b0;
+RST_tb = 1'b0;
+IN_tb  = 1'b0;
+#10
+RST_tb = 1'b1;
+
+#20 IN_tb = 1'b0;
+#20 IN_tb = 1'b1;
+#20 IN_tb = 1'b0;
+#10 IN_tb = 1'b1;
+#15 IN_tb = 1'b0;
+#15 IN_tb = 1'b1;
+#200
+$stop;
+end
+
+
+endmodule
